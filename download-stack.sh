47299822df61d4c9ddb89e444dc20d4f6cba5ca1
Andrew Anderson <anderson@elastic.co>
2018-12-16 21:53:37 -0500

    Cleaning up, separating components. added #TODOs

--- download-stack.sh
+++ download-stack.sh
@@ -10,58 +10,71 @@ ES_STACK_VERSION=${ES_STACK_VERSION}
 #TODO Move ./stack-download into the version directory directly
 #TODO Optional installs: 
 # - minio
-mv stack-download stack-download-`date '+%Y-%m-%d %H:%M:%S'`
+#mv stack-download stack-download-`date '+%Y-%m-%d %H:%M:%S'`
 mkdir stack-download
 cd stack-download
 # Elasticsearch
-wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-${ES_STACK_VERSION}.tar.gz
+#wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-${ES_STACK_VERSION}.tar.gz
 
 # Kibana
-wget https://artifacts.elastic.co/downloads/kibana/kibana-${ES_STACK_VERSION}-darwin-x86_64.tar.gz
-tar xvfz kibana-${ES_STACK_VERSION}-darwin-x86_64.tar.gz
-ln -s kibana-${ES_STACK_VERSION}-darwin-x86_64 kibana
+#wget https://artifacts.elastic.co/downloads/kibana/kibana-${ES_STACK_VERSION}-darwin-x86_64.tar.gz
+#tar xvfz kibana-${ES_STACK_VERSION}-darwin-x86_64.tar.gz
+
 
 # Logstash
-wget https://artifacts.elastic.co/downloads/logstash/logstash-${ES_STACK_VERSION}.tar.gz
+#wget https://artifacts.elastic.co/downloads/logstash/logstash-${ES_STACK_VERSION}.tar.gz
 
 # Beats
 # Filebeat
-wget https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-${ES_STACK_VERSION}-darwin-x86_64.tar.gz
+#wget https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-${ES_STACK_VERSION}-darwin-x86_64.tar.gz
 
 # Packetbeat
 wget https://artifacts.elastic.co/downloads/beats/packetbeat/packetbeat-${ES_STACK_VERSION}-darwin-x86_64.tar.gz
 
 # Metricbeat
 
-wget https://artifacts.elastic.co/downloads/beats/metricbeat/metricbeat-${ES_STACK_VERSION}-darwin-x86_64.tar.gz
+#wget https://artifacts.elastic.co/downloads/beats/metricbeat/metricbeat-${ES_STACK_VERSION}-darwin-x86_64.tar.gz
 
 # Heartbeat
-wget https://artifacts.elastic.co/downloads/beats/heartbeat/heartbeat-${ES_STACK_VERSION}-darwin-x86_64.tar.gz
+#wget https://artifacts.elastic.co/downloads/beats/heartbeat/heartbeat-${ES_STACK_VERSION}-darwin-x86_64.tar.gz
 
 # Auditbeat
-wget https://artifacts.elastic.co/downloads/beats/auditbeat/auditbeat-${ES_STACK_VERSION}-darwin-x86_64.tar.gz
+#wget https://artifacts.elastic.co/downloads/beats/auditbeat/auditbeat-${ES_STACK_VERSION}-darwin-x86_64.tar.gz
 
 # Functionbeat
-wget https://artifacts.elastic.co/downloads/beats/x-pack/functionbeat/functionbeat-${ES_STACK_VERSION}-darwin-x86_64.tar.gz
+#wget https://artifacts.elastic.co/downloads/beats/x-pack/functionbeat/functionbeat-${ES_STACK_VERSION}-darwin-x86_64.tar.gz
 
 # Journalbeat
-wget https://artifacts.elastic.co/downloads/beats/journalbeat/journalbeat-${ES_STACK_VERSION}-linux-x86_64.tar.gz
+#wget https://artifacts.elastic.co/downloads/beats/journalbeat/journalbeat-${ES_STACK_VERSION}-linux-x86_64.tar.gz
 echo .
 sleep 1
 echo .
 sleep 1
 cd stack-download
+# Explode tarballs and zip files
 for file in *.tar.gz; do tar xvfz $file; done;
 for file in *zip;  do unzip $file; done;
 sleep 1
+# Ceate Symlinks
+ln -s kibana-${ES_STACK_VERSION}-darwin-x86_64 kibana
 ln -s elasticsearch-${ES_STACK_VERSION} elasticsearch
 ln -s logstash-${ES_STACK_VERSION} logstash
-ln -s filebeat-${ES_STACK_VERSION} filebeat
-ln -s metricbeat-${ES_STACK_VERSION} metricbeat
-ln -s packetbeat-${ES_STACK_VERSION} packetbeat
-ln -s auditbeat-${ES_STACK_VERSION} auditbeat
+ln -s filebeat-${ES_STACK_VERSION}-darwin-x86_64 filebeat
+ln -s metricbeat-${ES_STACK_VERSION}-darwin-x86_64 metricbeat
+ln -s packetbeat-${ES_STACK_VERSION}-darwin-x86_64 packetbeat
+ln -s auditbeat-${ES_STACK_VERSION}-darwin-x86_64 auditbeat
 ln -s heartbeat-${ES_STACK_VERSION} heartbeat
-ln -s journal-${ES_STACK_VERSION} journalbeat
+ln -s journalbeat-${ES_STACK_VERSION}-linux-x86_64 journalbeat
+
+cd ..
+
+# Remove `current` symlink
+rm current
+# Move stack downloaded to the verion folder
+mv stack-download $ES_STACK_VERSION
+# Create new `current` symlink
+ln -s $ES_STACK_VERSION current
+
 
 # Other Tools
 
@@ -83,3 +96,5 @@ wget https://dl.minio.io/server/minio/release/darwin-amd64/minio
 chmod +x minio
 # to-run
 # ./minio server /data/minio
+
+
